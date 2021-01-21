a83d314f7d91e26290084ee244fdd093df916a91
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-01-21 10:47:19 +0100

    Made id edit editable

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
 // TODO
 // JSkod för sifferinputs fönstret
 // JSkod för blogginlägg
+// maxlängd för edits?
 
+window.onload = function(){
+    let editable = document.getElementById("edit");
+
+    makeEditable(edit);
+}
+
+function makeEditable(elem){
+    elem.onclick = function(){
+        elem.contentEditable = true;
+        elem.focus();
+    }
+}
