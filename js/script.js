b7dbcf9004acd5d53b9ea589f18dca25e983a8bc
Magnus Lindgren <magnus.c.lindgren@gmail.com>
2021-02-01 09:54:33 +0100

    Lagt till kommentarer och tömt sidan vid laddning

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-/*  Vad jag gjort för att felsöka och undvika buggar
+/*  
+    Vad jag gjort för att felsöka och undvika buggar
     Jag har använt mig av console.log för att spåra och se till att mina variablar
     får just det värde jag vill att det ska få. Jag har följt vad som händer i 
     webbläsarens konsolfönster för att se att rätt värde sätts in i variabeln. 
@@ -10,6 +11,11 @@
     och kollat om det fungerar för varje steg med hjälp av bland annat console.log.
 */
 
+/*
+    Här har jag eventet onload som jag använder för att köra igång mina functioner.
+    onload väntar med att köra tills allt på sidan är inladdat och klart innan det kör 
+    till skillnad mot  DOMContentLoaded som kör när DOM är färdigladdat. 
+*/
 window.onload = function(){
     document.getElementById("inputNumber").oninput = function(){
         let inputNumber = parseInt(this.value);
@@ -21,6 +27,10 @@ window.onload = function(){
 }
 
 //funktion för att göra ett element redigerbart
+/*  
+    onclick här är ett event. När man klickar i det valda området (väljs via parametern) så gör det
+    elementet redigerbart. 
+*/
 function makeEditable(box){
     box.onclick = function(){
         box.contentEditable = true;
@@ -37,7 +47,7 @@ function createBlogBox(input){
         let title = document.createElement("h4");
         let paragraph = document.createElement("p");
 
-        let button = document.createElement("button");
+        let button = document.createElement("button"); // Sparknapp
 
         //Skriv ut temp text i boxen
         title.innerText = "BlogPost " + (i+1);
@@ -49,13 +59,23 @@ function createBlogBox(input){
         makeEditable(title);
         makeEditable(paragraph);
 
-        //Skapa allt på sidan
+        //Skapa allt på sidan. 
+        /*
+            append lägger till node object i föräldren man specificerar.
+            förälder.append(objektet man lägger till).
+            Som i "section.append(title)" så skapar den en h4 (Jag skapade let title ovan som en h4) i section.
+        */ 
         section.append(title);
         section.append(paragraph);
         section.append(button)
         main.append(section);
 
         // Lägg till CSS
+        /*
+            Här lägger jag till styling från CSS filen till objekt jag skapat ovan
+            Exempel:
+            section.classList.add("textbox") här lägger jag till klassen textbox till section
+        */
         section.classList.add("textbox");
         button.classList.add("button");
     }
